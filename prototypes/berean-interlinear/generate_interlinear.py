a945bcbeb07f9b2833855b158f4dec6f3a18fa26
James Tauber <jtauber@jtauber.com>
2017-03-29 02:14:12 -0400

    make the berean generator closer to the other generators

--- prototypes/berean-interlinear/generate_interlinear.py
+++ prototypes/berean-interlinear/generate_interlinear.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
-from berean import load_interlinear, get_verse
-
+import os
+import shutil
 
 from jinja2 import Environment, FileSystemLoader
+
+from berean import load_interlinear, get_verse
 
 
 env = Environment(
@@ -12,13 +14,33 @@ env = Environment(
 template = env.get_template("template.html")
 
 
-def output(title, bcv):
+def generate(title, bcv, output_filename):
+
     entries = load_interlinear()
-    print(template.render(
-        title=title,
-        rows=get_verse(entries, bcv),
-    ))
+
+    with open(output_filename, "w") as output:
+        print(template.render(
+            title=title,
+            rows=get_verse(entries, bcv),
+        ), file=output)
+
+
+OUTPUT_DIR = "output"
 
 
 if __name__ == "__main__":
-    output("John 3.16", "040316")
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+        print(f"created {OUTPUT_DIR}")
+
+    book_name = "John"
+    book_num = 4
+    chapter_num = 3
+    verse_num = 16
+
+    title = f"{book_name} {chapter_num}.{verse_num}"
+    bcv = f"{book_num:02d}{chapter_num:02d}{verse_num:02d}"
+
+    output_filename = os.path.join(OUTPUT_DIR, f"{bcv}.html")
+    generate(title, bcv, output_filename)
+    print(f"wrote {output_filename}")
