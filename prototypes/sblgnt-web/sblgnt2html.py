93e748f42777a12c640023561d73b76ec36e418c
James Tauber <jtauber@jtauber.com>
2017-03-28 20:40:33 -0400

    cleaned up parse code rendering

--- prototypes/sblgnt-web/sblgnt2html.py
+++ prototypes/sblgnt-web/sblgnt2html.py
@@ -25,111 +25,103 @@ def render_pos(row):
     }[row["ccat-pos"]]
 
 
+def render_person(row):
+    return {
+        "1": "1st person",
+        "2": "2nd person",
+        "3": "3rd person",
+    }[row["ccat-parse"][0]]
+
+
+def render_tense(row):
+    return {
+        "P": "present",
+        "F": "future",
+        "A": "aorist",
+        "X": "perfect",
+        "Y": "pluperfect",
+        "I": "imperfect",
+    }[row["ccat-parse"][1]]
+
+
+def render_voice(row):
+    return {
+        "A": "active",
+        "M": "middle",
+        "P": "passive",
+    }[row["ccat-parse"][2]]
+
+
+def render_mood(row):
+    return {
+        "I": "indicative",
+        "S": "subjunctive",
+        "D": "imperative",
+        "O": "optative",
+    }[row["ccat-parse"][3]]
+
+
+def render_case(row):
+    return {
+        "N": "nominative",
+        "A": "accusative",
+        "G": "genitive",
+        "D": "dative",
+        "V": "vocative",
+        "-": "",
+    }[row["ccat-parse"][4]]
+
+
+def render_number(row):
+    return {
+        "S": "singular",
+        "P": "plural",
+        "-": "",
+    }[row["ccat-parse"][5]]
+
+
+def render_gender(row):
+    return {
+        "M": "masculine",
+        "F": "feminine",
+        "N": "neuter",
+        "-": "",
+    }[row["ccat-parse"][6]]
+
+
+def render_degree(row):
+    return {
+        "C": "comparative",
+        "S": "superlative",
+        "-": "",
+    }[row["ccat-parse"][7]]
+
+
 def render_parse(row):
-    pos = row["ccat-pos"]
-    person, tense, voice, mood, case, number, gender, degree = row["ccat-parse"]
     parse = []
-    if pos == "V-":
-        if mood == "P":
-            parse.append({
-                "P": "present",
-                "A": "aorist",
-                "X": "perfect",
-                "F": "future",
-            }[tense])
-            parse.append({
-                "A": "active",
-                "M": "middle",
-                "P": "passive",
-            }[voice])
+    if row["ccat-pos"] == "V-":
+        if row["ccat-parse"][3] == "P":
+            parse.append(render_tense(row))
+            parse.append(render_voice(row))
             parse.append("participle")
-            parse.append({
-                "N": "nominative",
-                "A": "accusative",
-                "G": "genitive",
-                "D": "dative",
-                "V": "vocative",
-                "-": "",
-            }[case])
-            parse.append({
-                "S": "singular",
-                "P": "plural",
-                "-": "",
-            }[number])
-            parse.append({
-                "M": "masculine",
-                "F": "feminine",
-                "N": "neuter",
-                "-": "",
-            }[gender])
-        elif mood == "N":
-            parse.append({
-                "P": "present",
-                "A": "aorist",
-                "X": "perfect",
-                "F": "future",
-            }[tense])
-            parse.append({
-                "A": "active",
-                "M": "middle",
-                "P": "passive",
-            }[voice])
+            parse.append(render_case(row))
+            parse.append(render_number(row))
+            parse.append(render_gender(row))
+        elif row["ccat-parse"][3] == "N":
+            parse.append(render_tense(row))
+            parse.append(render_voice(row))
             parse.append("infinitive")
         else:
-            parse.append({
-                "P": "present",
-                "F": "future",
-                "A": "aorist",
-                "X": "perfect",
-                "Y": "pluperfect",
-                "I": "imperfect",
-            }[tense])
-            parse.append({
-                "A": "active",
-                "M": "middle",
-                "P": "passive",
-            }[voice])
-            parse.append({
-                "I": "indicative",
-                "S": "subjunctive",
-                "D": "imperative",
-                "O": "optative",
-            }[mood])
-            parse.append({
-                "1": "1st person",
-                "2": "2nd person",
-                "3": "3rd person",
-            }[person])
-            parse.append({
-                "S": "singular",
-                "P": "plural",
-                "-": "",
-            }[number])
+            parse.append(render_tense(row))
+            parse.append(render_voice(row))
+            parse.append(render_mood(row))
+            parse.append(render_person(row))
+            parse.append(render_number(row))
     else:
-        parse.append({
-            "N": "nominative",
-            "A": "accusative",
-            "G": "genitive",
-            "D": "dative",
-            "V": "vocative",
-            "-": "",
-        }[case])
-        parse.append({
-            "S": "singular",
-            "P": "plural",
-            "-": "",
-        }[number])
-        parse.append({
-            "M": "masculine",
-            "F": "feminine",
-            "N": "neuter",
-            "-": "",
-        }[gender])
-        parse.append({
-            "C": "comparative",
-            "S": "superlative",
-            "-": "",
-        }[degree])
+        parse.append(render_case(row))
+        parse.append(render_number(row))
+        parse.append(render_gender(row))
+        parse.append(render_degree(row))
 
     return " ".join(parse).strip()
 
@@ -204,7 +196,8 @@ if not os.path.exists(OUTPUT_DIR):
     print(f"created {OUTPUT_DIR}")
 
 for i, book_name in enumerate(BOOK_NAMES):
-    output_filename = os.path.join(OUTPUT_DIR, f"{book_name.replace(' ', '').lower()}.html")
+    output_filename = os.path.join(
+        OUTPUT_DIR, f"{book_name.replace(' ', '').lower()}.html")
     generate(book_name, i + 1, output_filename)
     print(f"wrote {output_filename}")
 
