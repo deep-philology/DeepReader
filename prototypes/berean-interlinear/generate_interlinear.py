bcb16230ed196a6dc3bb14b564fd5818176dca7f
James Tauber <jtauber@jtauber.com>
2017-03-29 01:57:21 -0400

    converted berean interlinear generator to Jinja2

--- prototypes/berean-interlinear/generate_interlinear.py
+++ prototypes/berean-interlinear/generate_interlinear.py
@@ -3,50 +3,22 @@
 from berean import load_interlinear, get_verse
 
 
+from jinja2 import Environment, FileSystemLoader
+
+
+env = Environment(
+    loader=FileSystemLoader("."),
+)
+template = env.get_template("template.html")
+
+
 def output(title, bcv):
-    print(f"""
-<html>
-<head>
-<title>{title}</title>
-<meta charset="utf-8">
-    """)
-    print("""
-<style>
-div.unit {
- float: left;
- margin-bottom: 1em;
- color: black;
-}
-p.gk {
- font-size: 16pt;
- margin: 0em;
- padding: 0em 0.5em;
-}
-p.en {
- font-size: 10pt;
- font-family: sans-serif;
- color: gray;
- margin: 0em;
- padding: 0em 1em;
-}
-</style>
-    """)
-    print(f"""
-</head>
-<body>
-<h1>{title}</h1>
-    """)
-
     entries = load_interlinear()
-    for row in get_verse(entries, bcv):
-        greek = row["greek"]
-        english = row["english"]
-        print(f"""<div class="unit"><p class="gk">{greek}</p><p class="en">{english}</p></div>""")
-
-    print("""
-    </body>
-    </html>
-    """)
+    print(template.render(
+        title=title,
+        rows=get_verse(entries, bcv),
+    ))
 
 
-output("John 3.16", "040316")
+if __name__ == "__main__":
+    output("John 3.16", "040316")
