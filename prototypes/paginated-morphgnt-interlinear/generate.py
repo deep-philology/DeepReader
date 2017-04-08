fb9f7f3447b34398655db9200fa74118288249e1
James Tauber <jtauber@jtauber.com>
2017-04-08 00:20:48 -0400

    refactoring of pagination and generation

--- prototypes/paginated-morphgnt-interlinear/generate.py
+++ prototypes/paginated-morphgnt-interlinear/generate.py
@@ -10,6 +10,24 @@ OUTPUT_DIR = "output"
 template = templates.load("template.html")
 
 
+def verse_filename(verse):
+    if verse:
+        return os.path.join(OUTPUT_DIR, f"{verse.verse_num}.html")
+
+
+def generate(verse, rows, prev_verse, next_verse, output_filename):
+    with open(output_filename, "w") as output:
+        print(template.render(
+            title=verse.title,
+            rows=[
+                {**row, "pos": morphgnt.pos(row), "parse": morphgnt.parse(row)}
+                for row in rows
+            ],
+            prev_file=verse_filename(prev_verse),
+            next_file=verse_filename(next_verse),
+        ), file=output)
+
+
 if __name__ == "__main__":
     fs.create_dir(OUTPUT_DIR)
 
@@ -18,28 +36,12 @@ if __name__ == "__main__":
     verses = morphgnt.rows_by_verses_for_chapter(chapter)
 
     for i, (verse, rows) in enumerate(verses):
-        output_filename = os.path.join(OUTPUT_DIR, f"{verse.verse_num}.html")
 
-        if i > 0:
-            prev_file = os.path.join(OUTPUT_DIR, f"{verses[i - 1][0].verse_num}.html")
-        else:
-            prev_file = None
+        prev_verse = verses[i - 1][0] if i > 0 else None
+        next_verse = verses[i + 1][0] if i < len(verses) - 1 else None
 
-        if i < len(verses) - 1:
-            next_file = os.path.join(OUTPUT_DIR, f"{verses[i + 1][0].verse_num}.html")
-        else:
-            next_file = None
-
-        with open(output_filename, "w") as output:
-            print(template.render(
-                title=verse.title,
-                rows=[
-                    {**row, "pos": morphgnt.pos(row), "parse": morphgnt.parse(row)}
-                    for row in rows
-                ],
-                prev_file=prev_file,
-                next_file=next_file,
-            ), file=output)
+        output_filename = verse_filename(verse)
+        generate(verse, rows, prev_verse, next_verse, output_filename)
         print(f"wrote {output_filename}")
 
     fs.copy_css(["interlinear.css", "pagination.css", "skolar.css"], OUTPUT_DIR)
