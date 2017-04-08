4afef7a2fb9ec0e8c11578e806d3637cfd6e1904
James Tauber <jtauber@jtauber.com>
2017-04-07 23:50:17 -0400

    made sblgnt-web generate more consistent with toggle-controls generate

--- prototypes/sblgnt-web/generate.py
+++ prototypes/sblgnt-web/generate.py
@@ -5,31 +5,29 @@ import os
 from reader import fs, templates, morphgnt, ref
 
 
+OUTPUT_DIR = "output"
+
 book_template = templates.load("template.html")
 
 
 def generate(book, output_filename):
 
-    book_content = morphgnt.rows_by_verses_by_chapters_for_book(book.num)
+    chapters = morphgnt.rows_by_verses_by_chapters_for_book(book.num)
 
     with open(output_filename, "w") as output:
         print(book_template.render(
             book_title=book.title,
-            book_content=book_content,
+            chapters=chapters,
             render_pos=morphgnt.render_pos,
             render_parse=morphgnt.render_parse,
         ), file=output)
-
-
-OUTPUT_DIR = "output"
 
 
 if __name__ == "__main__":
     fs.create_dir(OUTPUT_DIR)
 
     for book in ref.NT_BOOKS:
-        output_filename = os.path.join(
-            OUTPUT_DIR, f"{book.filename}.html")
+        output_filename = os.path.join(OUTPUT_DIR, f"{book.filename}.html")
         generate(book, output_filename)
         print(f"wrote {output_filename}")
 
