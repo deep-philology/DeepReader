9b1ce0b049a525134b9948047a8877b7ab30e5a1
James Tauber <jtauber@jtauber.com>
2017-04-21 16:42:05 -0400

    JSON generator for perseus4 example

---
+++ prototypes/static-json-perseus4/generate.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import json
+import os
+
+from reader import fs
+from reader.pagination import paginate
+
+from parse_tei2 import tei_chapters
+
+
+OUTPUT_DIR = "output"
+
+
+def chapter_filename(num_content):
+    "takes tuple of (chapter_num, chapter_content)"
+    if num_content:
+        return f"{num_content[0]}.json"
+
+
+def generate(chapter, prev, nxt, output_filename):
+    chapter_num, chapter_contents = chapter
+    with open(output_filename, "w") as output:
+        json.dump(dict(
+            title=f"Histories 2.{chapter_num}",
+            content=chapter_contents,
+            prev_file=chapter_filename(prev),
+            next_file=chapter_filename(nxt),
+        ), output)
+
+
+if __name__ == "__main__":
+    fs.create_dir(OUTPUT_DIR)
+
+    chapters = tei_chapters(os.path.join("data", "histories2.xml"))
+
+    for prev, item, nxt in paginate(chapters):
+        output_filename = os.path.join(OUTPUT_DIR, chapter_filename(item))
+        generate(item, prev, nxt, output_filename)
+        print(f"wrote {output_filename}")
