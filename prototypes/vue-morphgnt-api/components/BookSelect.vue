d538e8cf32e35c72c4a36f2f965e0e4dc9aea7ba
James Tauber <jtauber@jtauber.com>
2017-04-27 20:26:06 -0400

    make book select widget collapsible

--- prototypes/vue-morphgnt-api/components/BookSelect.vue
+++ prototypes/vue-morphgnt-api/components/BookSelect.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="widget">
-    <header>Book</header>
-    <section>
+    <header @click.prevent="toggle">Book</header>
+    <section v-if="open">
       <ul>
         <li v-for="book in books"><nuxt-link :to="{ query: {book: book['@id']} }">{{ book.name }}</nuxt-link></li>
       </ul>
@@ -11,13 +11,26 @@
 
 <script>
 export default {
+  data () {
+    return {
+      open: true
+    }
+  },
   props: [
     'books'
-  ]
+  ],
+  methods: {
+    toggle () {
+      this.open = !this.open
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+  header {
+    cursor: pointer;
+  }
   a {
     display: block;
     color: inherit;
