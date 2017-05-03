ff345ea89510df2fef2fb0ea113681ddbb811733
James Tauber <jtauber@jtauber.com>
2017-05-02 21:41:04 -0400

    added JSON object display component and use it in Morpheus

---
+++ prototypes/vue-morphgnt-api/components/JsonObject.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-for="value,key in obj">
+      <b>{{ key }}</b>:
+      <span v-if="typeof(value) === 'string'">{{ value }}</span>
+      <div v-else class="sublist">
+        <div v-for="subvalue,subkey in value">
+          <b>{{ subkey }}</b>: {{ subvalue }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: [
+      'obj'
+    ]
+  }
+</script>
+
+<style lang="scss" scoped>
+  .sublist {
+    margin-left: 1em;
+  }
+</style>
--- prototypes/vue-morphgnt-api/components/Morpheus.vue
+++ prototypes/vue-morphgnt-api/components/Morpheus.vue
@@ -1,11 +1,15 @@
 <template>
   <widget>
     <span slot="header">Morpheus</span>
-    <span slot="summary" class="word-info-summary">
-      {{ word }}
+    <span slot="summary">
+      {{ word.word }}
     </span>
-    <div slot="body" class="word-info-body">
-      {{ word }}
+    <div slot="body" v-if="morphEntry">
+      <json-object :obj="morphEntry.dict"></json-object>
+      <div v-if="Array.isArray(morphEntry.infl)">
+        <json-object v-for="item,index in morphEntry.infl" class="item" :obj="item"></json-object>
+      </div>
+      <json-object v-else :obj="morphEntry.infl" class="item"></json-object>
     </div>
   </widget>
 </template>
@@ -13,6 +17,7 @@
 <script>
   import axios from 'axios'
   import Widget from '~components/Widget.vue'
+  import JsonObject from '~components/JsonObject.vue'
 
   export default {
     mounted () {
@@ -20,20 +25,27 @@
         this.word = word
         const url = `http://services.perseids.org/bsp/morphologyservice/analysis/word?word=${word.word}&lang=grc&engine=morpheusgrc`
         axios.get(url).then((response) => {
-          this.word = response.data
+          this.morphEntry = response.data.RDF.Annotation.Body.rest.entry
         })
       })
     },
     data () {
       return {
-        word: ''
+        word: '',
+        morphEntry: null
       }
     },
     components: {
-      Widget
+      Widget,
+      JsonObject
     }
   }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+  .item {
+    margin: 10px 0;
+    padding-left: 10px;
+    border-left: 2px solid #CCC;
+  }
 </style>
