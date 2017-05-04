4313062392900c73aaae9f254c07de224366b6b3
Brian Rosner <brian@brosner.com>
2017-05-04 14:05:45 -0600

    turn off auto browse load

--- prototypes/vue-morphgnt-api/config/index.js
+++ prototypes/vue-morphgnt-api/config/index.js
@@ -24,7 +24,7 @@ module.exports = {
   dev: {
     env: require('./dev.env'),
     port: 5066,
-    autoOpenBrowser: true,
+    autoOpenBrowser: false,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
