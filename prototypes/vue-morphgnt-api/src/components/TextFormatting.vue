85f6920a9c23cefdfe8a1796a48b9d22fd204e41
James Tauber <jtauber@jtauber.com>
2017-05-05 19:49:01 -0400

    text formatting style tweaks

--- prototypes/vue-morphgnt-api/src/components/TextFormatting.vue
+++ prototypes/vue-morphgnt-api/src/components/TextFormatting.vue
@@ -2,9 +2,9 @@
   <widget>
     <span slot="header">Text Formatting</span>
     <div slot="body">
-      <div @click="smallerText">Smaller Text</div>
-      <div @click="normalText">Normal Text</div>
-      <div @click="largerText">Larger Text</div>
+      <div class="click" @click="smallerText">Smaller Text</div>
+      <div class="click" @click="normalText">Normal Text</div>
+      <div class="click" @click="largerText">Larger Text</div>
     </div>
   </widget>
 </template>
@@ -15,13 +15,13 @@
   export default {
     methods: {
       smallerText() {
-        this.$store.commit('setTextSize', '10pt');
+        this.$store.commit('setTextSize', '14pt');
       },
       normalText() {
-        this.$store.commit('setTextSize', '20pt');
+        this.$store.commit('setTextSize', '16pt');
       },
       largerText() {
-        this.$store.commit('setTextSize', '30pt');
+        this.$store.commit('setTextSize', '20pt');
       },
     },
     components: {
@@ -29,3 +29,14 @@
     },
   };
 </script>
+
+<style lang="scss" scoped>
+  div.click {
+    color: inherit;
+    text-decoration: none;
+    cursor: pointer;
+    &:hover {
+      color: #666;
+    }
+  }
+</style>
