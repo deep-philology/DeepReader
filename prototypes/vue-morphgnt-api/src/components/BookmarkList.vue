f47dccd7a7864eb6a139bda749fc31cdb69243f0
Brian Rosner <brian@brosner.com>
2017-05-05 13:07:46 -0600

    added BookmarkList.vue file (missing from previous commit)

---
+++ prototypes/vue-morphgnt-api/src/components/BookmarkList.vue
@@ -0,0 +1,79 @@
+<template>
+  <widget>
+    <span slot="header">Bookmarks</span>
+    <div slot="body">
+      <button v-if="passage" @click="saveBookmark">save</button>
+      <div v-for="bookmark in bookmarks" class="bookmark">
+        <span class="remove" @click="removeBookmark(bookmark)">remove</span>
+        <router-link :to="$parent.passageLink($parent.query, bookmark.resource)">{{ bookmark.title }}</router-link>
+      </div>
+    </div>
+  </widget>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+  import Widget from '@/components/Widget';
+  import app from '@/firebase';
+
+  const db = app.database();
+  const bookmarksRef = db.ref('bookmarks');
+
+  export default {
+    created() {
+      this.$store.dispatch('setBookmarksRef', this.userBookmarks());
+    },
+    computed: {
+      passage() {
+        return this.$parent.passage;
+      },
+      ...mapGetters(['user', 'bookmarks']),
+    },
+    methods: {
+      userBookmarks() {
+        return bookmarksRef.child(this.user.uid);
+      },
+      saveBookmark() {
+        this.userBookmarks().push({
+          resource: this.passage['@id'],
+          title: this.passage.title,
+        });
+      },
+      removeBookmark(bookmark) {
+        this.userBookmarks().child(bookmark['.key']).set(null);
+      },
+    },
+    components: {
+      Widget,
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  a {
+    display: block;
+    color: inherit;
+    text-decoration: none;
+    padding: 2px 5px;
+    &:hover {
+      background: #DDD;
+    }
+  }
+  .bookmark {
+    padding: 2px 5px 2px 0;
+    .remove {
+      color: transparent;
+      float: right;
+      cursor: pointer;
+    }
+    &:hover {
+      .remove {
+        transition: color 0.2s ease-in-out;
+        color: inherit;
+        &:hover {
+          color: #C00;
+        }
+      }
+    }
+  }
+</style>
