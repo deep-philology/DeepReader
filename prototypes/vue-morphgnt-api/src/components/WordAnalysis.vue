4b6e349603a857f8616807dd5fa94509d164fe7a
James Tauber <jtauber@jtauber.com>
2017-05-06 00:58:04 -0400

    improved spacing in WordAnalysis

--- prototypes/vue-morphgnt-api/src/components/WordAnalysis.vue
+++ prototypes/vue-morphgnt-api/src/components/WordAnalysis.vue
@@ -2,9 +2,7 @@
   <div>
     <div class="norm" @click="handleNormSelect(word)">{{ word.norm }}&nbsp;</div>
     <div class="analysis">
-      <span class="pos">{{ word.pos }}</span>
-      <span v-if="word.mood">.{{ word.tense }}{{ word.voice }}{{ word.mood }}</span>
-      <span v-if="word.number">.{{ word.person }}{{ word.case }}{{ word.number }}{{ word.gender }}</span>
+      <span class="pos">{{ word.pos }}</span><span v-if="word.mood">.{{ word.tense }}{{ word.voice }}{{ word.mood }}</span><span v-if="word.number">.{{ word.person }}{{ word.case }}{{ word.number }}{{ word.gender }}</span>
     </div>
     <div class="lemma">{{ word.lemma }}</div>
   </div>
