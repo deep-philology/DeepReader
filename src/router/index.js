c8376c61bd4c9ee738b9e4253106b9aa54b16c1a
John D. Lewis <balinjdl+github@gmail.com>
2017-06-24 09:26:58 -0400

    Created homepage
    Created simple homepage

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,17 @@ import Router from 'vue-router';
 import CTSReader from '@/cts/components/Reader';
 import MorphGNTReader from '@/morphgnt/components/Reader';
 import CiteServicesReader from '@/cite-services/components/Reader';
+import Homepage from '@/components/Homepage';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'Homepage',
+      component: Homepage,
+    },
     {
       path: '/cts',
       name: 'CTSReader',
